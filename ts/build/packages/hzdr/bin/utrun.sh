44e486214c06b72c1f7b1ca587eca6ce954cfe72
Jens Maus <mail@jens-maus.de>
2015-06-23 14:25:16 +0200

    update old qutchooser in hzdr package and corresponding startip scripts. added openssh stuff to gitignore.

--- ts/build/packages/hzdr/bin/utrun.sh
+++ ts/build/packages/hzdr/bin/utrun.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-UTBIN=/bin
+UTBIN=/usr/bin
 LOG="${HOME}/pfx.log"
 
 UTOU="ou=uttoken,ou=utdata,o=fsr,dc=de"
@@ -34,63 +34,63 @@ then
     
     if [ $GR = 0 ] && [ "$RTYPE" = "reconnect" ];
     then
-	HOST=`grep radiusLoginIPHost $LDIF | awk '{ print $2; }'`
-	STYPE=`grep radiusServiceType $LDIF | awk '{ print $2; }'`
-	case $STYPE in
-	    NX) 
-		APP="xterm -fn 10x20"
+        HOST=`grep radiusLoginIPHost $LDIF | awk '{ print $2; }'`
+        STYPE=`grep radiusServiceType $LDIF | awk '{ print $2; }'`
+        case $STYPE in
+            NX) 
+                APP="xterm -fn 10x20"
                 HOST="lts1"
-		;;
-	    RDP) 
-		APP="rdesktop"
-		;;
-	    APP) 
-		APP="firefox"
-		;;
+                ;;
+            RDP) 
+                APP="rdesktop"
+                ;;
+            APP) 
+                APP="firefox"
+                ;;
         esac
-	echo "Session of type $STYPE for Payflex.$PID exists on host $HOST" >> $LOG 2>&1
+        echo "Session of type $STYPE for Payflex.$PID exists on host $HOST" >> $LOG 2>&1
         $UTBIN/utupdate.sh $RTYPE $PID $HOST >> $LOG 2>&1
     else
-	case $RTYPE in
-	    0) 
-		HOST="lts1"
-		STYPE="NX"
-		APP="xterm -fn 10x20"
-		;;
-	    1) 
-		HOST="xats"
-		STYPE="RDP"
-		APP="rdesktop"
-		;;
-	    2) 
-		HOST="$HOSTNAME"
-		STYPE="APP"
-		APP="firefox"
-		;;
+        case $RTYPE in
+            0) 
+                HOST="lts1"
+                STYPE="NX"
+                APP="nx"
+                ;;
+            1) 
+                HOST="xats"
+                STYPE="RDP"
+                APP="rdesktop"
+                ;;
+            2) 
+                HOST="$HOSTNAME"
+                STYPE="APP"
+                APP="firefox"
+                ;;
         esac
-	echo "new session of type $STYPE for $PID on host $HOST" >> $LOG 2>&1
+        echo "new session of type $STYPE for $PID on host $HOST" >> $LOG 2>&1
     fi
 else
     UNAME="kiosk"
     PID="`nodename -n`"
     case $RTYPE in
-	0) 
-	    HOST="lts1"
-	    #		STYPE="NX"
-	    # for testing ...
-	    STYPE="NX"
-	    APP="xterm -fn 10x20"
-	    ;;
-	1) 
-	    HOST="xats"
-	    STYPE="RDP"
-	    APP="rdesktop"
-	    ;;
-	2) 
-	    HOST="$HOSTNAME"
-	    STYPE="APP"
-  	    APP="firefox"
-	    ;;
+        0) 
+            HOST="lts1"
+            #           STYPE="NX"
+            # for testing ...
+            STYPE="NX"
+            APP="nx"
+            ;;
+        1) 
+            HOST="xats"
+            STYPE="RDP"
+            APP="rdesktop"
+            ;;
+        2) 
+            HOST="$HOSTNAME"
+            STYPE="APP"
+            APP="firefox"
+            ;;
     esac
     echo "new session of type $STYPE for $PID on host $HOST" >> $LOG 2>&1
 fi
@@ -100,27 +100,51 @@ echo "EXEC: $STYPE @ $HOST : $APP" >> $LOG 2>&1
 
 case $STYPE in
     RDP)
-	#rdesktop -u $UNAME -d FZR -f $HOST & >> $LOG 2>&1
-  xfreerdp /u:$UNAME /d:FZR /f $HOST & >> $LOG 2>&1
-	CPID=$!
-	;;
+        if [ "$UNAME" != "" ];
+        then
+           UNP = "/u:$UNAME"
+        else
+           UNP = ""
+        fi
+        xfreerdp $UNP /d:FZR /f /v:$HOST & >> $LOG 2>&1
+        CPID=$!
+        echo $CPID > /tmp/ut.pid
+        ;;
     NX)
-	/usr/lib/thinlinc/bin/tlclient -C /etc/tlclient.cfg -x -h options,controlpanel -u $UNAME $HOST & >> $LOG 2>&1
-	CPID=$!
-	$UTBIN/tlcstartover.sh
-	#$UTBIN/mkfullscreen.sh $HOST >> $LOG 2>&1 &
-        # it looks like login was o.k., remove user key
-	;;
+        if [ "$UNAME" != "" ];
+        then
+            UNP="-u $UNAME"
+        else
+            UNP=""
+        fi
+        if [ "$3" != "password" ];
+        then
+            echo "TLC:pubkey"
+            # enable AUTOLOGIN=1 in config file!
+            /usr/lib/thinlinc/bin/tlclient -C /etc/tlc-pubkey.cfg -x -h options,controlpanel -u $UNAME $HOST & >> $LOG 2>&1
+        else
+            echo "TLC:password"
+            /usr/lib/thinlinc/bin/tlclient -C /etc/tlc-password.cfg -x -h options,controlpanel -u $UNAME $HOST & >> $LOG 2>&1
+        fi 
+        CPID=$!
+        sleep 1 
+        ps | grep -v grep | grep tlclient.bin | awk '{ print $1; }' > /tmp/ut.pid
+        rm /tmp/user.key
+        ;;
     APP)
-#	$UTBIN/apprun.sh "$APP" "$UNAME" "$HOST" "$PID" & >> $LOG
-	$APP & >> $LOG 2>&1
-	CPID=$!
-	;;
+#       $UTBIN/apprun.sh "$APP" "$UNAME" "$HOST" "$PID" & >> $LOG
+        $APP & >> $LOG 2>&1
+        CPID=$!
+        echo $CPID > /tmp/ut.pid
+        ;;
 esac
 
-
+sleep 5
+rm -f /tmp/user.key
+CPID=`cat /tmp/ut.pid`
+ps >> $LOG 2>&1
 echo "Childprocess started as $CPID over $MPID" >> $LOG 2>&1
-while [ `ps | grep $CPID | grep -v grep > /dev/null; echo $?` = 0 ]; do
+while [ "`ps ax | grep $CPID | grep -v grep > /dev/null; echo $?`" = "0" ]; do
     sleep 1
 done
 
@@ -134,3 +158,4 @@ then
 fi
 
 exit 0
+
