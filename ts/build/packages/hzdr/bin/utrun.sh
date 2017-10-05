b7d43bf644893dcd84b24f7c1cda3a8556676e95
Jens Lasch <j.lasch@hzdr.de>
2017-10-05 14:19:52 +0200

    new chooser by N. Schmeisser

--- ts/build/packages/hzdr/bin/utrun.sh
+++ ts/build/packages/hzdr/bin/utrun.sh
@@ -1,7 +1,8 @@
-#!/bin/sh
+#!/bin/bash
 
 UTBIN=/usr/bin
-LOG="${HOME}/pfx.log"
+TLCBIN=/usr/lib
+LOG="/home/tsuser/pfx.log"
 
 UTOU="ou=uttoken,ou=utdata,o=fsr,dc=de"
 UTSD="ou=utsession,ou=utdata,o=fsr,dc=de"
@@ -9,17 +10,26 @@ UTSD="ou=utsession,ou=utdata,o=fsr,dc=de"
 LDAP=ldap.fz-rossendorf.de
 LDIF=/tmp/$$.ldif
 
-#export LD_LIBRARY_PATH=$UTBIN
+export LD_LIBRARY_PATH=$UTBIN
 
 RTYPE=$1
 PID=$2
-ETYPE=0
+HOST=$3
+export ETYPE=0
+
+#function finish
+#{
+#   echo "Interrupting session for $PID ($MPID) with child $CPID" >> $LOG 2>&1
+#   $UTBIN/tlckill.sh >> $LOG 2>&1
+#   $UTBIN/utupdate.sh 255 $PID >> $LOG 2>&1
+#   export ETYPE=2
+#}
 
 #--- TRAP handling for clean exit --------------------
-MPID=$$
-CPID=0
-#trap 'echo "Interrupting session for $PID ($MPID) with child $CPID" >> $LOG 2>&1; $UTBIN/tlckill.sh >> $LOG 2&>1; jobs -l >> $LOG 2>&1; kill -9 $CPID >> $LOG 2>&1; exit 0' INT TERM
-trap 'echo "Interrupting session for $PID ($MPID) with child $CPID" >> $LOG 2>&1; $UTBIN/tlckill.sh >> $LOG 2&>1; $UTBIN/utupdate.sh 255 $PID >> $LOG 2>&1; ETYPE=1' INT TERM
+export MPID=$$
+export CPID=0
+trap 'echo "Interrupting session for $PID ($MPID) with child $CPID" >> $LOG 2>&1; $UTBIN/tlckill.sh >> $LOG 2>&1; $UTBIN/utupdate.sh 255 $PID >> $LOG 2>&1; ETYPE=2' SIGINT SIGTERM
+#trap finish SIGINT SIGTERM
 #-----------------------------------------------------
 
 if [ "$PID" != "" ];
@@ -34,127 +44,136 @@ then
     
     if [ $GR = 0 ] && [ "$RTYPE" = "reconnect" ];
     then
-        HOST=`grep radiusLoginIPHost $LDIF | awk '{ print $2; }'`
-        STYPE=`grep radiusServiceType $LDIF | awk '{ print $2; }'`
-        case $STYPE in
-            NX) 
-                APP="xterm -fn 10x20"
-                HOST="lts1"
-                ;;
-            RDP) 
-                APP="rdesktop"
-                ;;
-            APP) 
-                APP="firefox"
-                ;;
+	HOST=`grep radiusLoginIPHost $LDIF | awk '{ print $2; }'`
+	STYPE=`grep radiusServiceType $LDIF | awk '{ print $2; }'`
+	case $STYPE in
+	    NX)
+		APP="tlc"
+		;;
+	    RDP)
+		APP="xfreerdp"
+		;;
+	    APP)
+		APP="firefox"
+		;;
         esac
-        echo "Session of type $STYPE for Payflex.$PID exists on host $HOST" >> $LOG 2>&1
+	echo "Session of type $STYPE for Payflex.$PID exists on host $HOST" >> $LOG 2>&1
         $UTBIN/utupdate.sh $RTYPE $PID $HOST >> $LOG 2>&1
     else
-        case $RTYPE in
-            0) 
-                HOST="lts1"
-                STYPE="NX"
-                APP="nx"
-                ;;
-            1) 
-                HOST="xats"
-                STYPE="RDP"
-                APP="rdesktop"
-                ;;
-            2) 
-                HOST="$HOSTNAME"
-                STYPE="APP"
-                APP="firefox"
-                ;;
+	case $RTYPE in
+	    0)
+		STYPE="NX"
+		APP="tlc"
+		;;
+	    1)
+		STYPE="RDP"
+		APP="xfreerdp"
+		;;
+	    2)
+		STYPE="APP"
+		APP="firefox"
+		;;
         esac
-        echo "new session of type $STYPE for $PID on host $HOST" >> $LOG 2>&1
+	echo "new session of type $STYPE for $PID on host $HOST" >> $LOG 2>&1
     fi
 else
-    UNAME="kiosk"
-    PID="`nodename -n`"
+#    UNAME="kiosk"
+#    PID="`nodename -n`"
+    PID="`hostname`"
     case $RTYPE in
-        0) 
-            HOST="lts1"
-            #           STYPE="NX"
-            # for testing ...
-            STYPE="NX"
-            APP="nx"
-            ;;
-        1) 
-            HOST="xats"
-            STYPE="RDP"
-            APP="rdesktop"
-            ;;
-        2) 
-            HOST="$HOSTNAME"
-            STYPE="APP"
-            APP="firefox"
-            ;;
-    esac
-    echo "new session of type $STYPE for $PID on host $HOST" >> $LOG 2>&1
+	0)
+	    #STYPE="NX"
+	    #for testing ...
+	    STYPE="NX"
+	    APP="tlc"
+	    ;;
+	1)
+	    STYPE="RDP"
+	    APP="xfreerdp"
+	    ;;
+	2)
+	    STYPE="APP"
+	    APP="firefox"
+	    ;;
+esac
+echo "new session of type $STYPE for $PID on host $HOST" >> $LOG 2>&1
 fi
 
 # execute some command here
-echo "EXEC: $STYPE @ $HOST : $APP" >> $LOG 2>&1
+echo "EXEC: $STYPE @ $HOST : $APP"# >> $LOG 2>&1
 
 case $STYPE in
     RDP)
-        if [ "$UNAME" != "" ];
+	PWDARG=`$UTBIN/utpwd $HOST $UNAME`
+
+        if [ "$?" != "0" ];
         then
-           UNP = "/u:$UNAME"
-        else
-           UNP = ""
-        fi
-        xfreerdp /f /multimon /kbd:German /d:FZR $UNP /t:${UNAME}@${HOST} /cert-ignore /drive:USB,/mnt/$(hostname)/ /sound:latency:400 /microphone:sys:pulse +fonts +window-drag -menu-anims -themes +wallpaper -toggle-fullscreen /v:${HOST} & >> $LOG 2>&1
-        CPID=$!
-        echo $CPID > /tmp/ut.pid
-        ;;
+	  PWD_UNAME=$(echo $PWDARG | cut -f1 -d~)
+	  PWD_PWD=$(echo $PWDARG | cut -f2 -d~)
+
+	  if [ "$PWD_UNAME" != "" ];
+	  then
+	      xfreerdp /cert-ignore /f /u:"$PWD_UNAME" /p:"$PWD_PWD" /d:FZR /v:$HOST & >> $LOG 2>&1
+	  else
+	      xfreerdp /cert-ignore /f                               /d:FZR /v:$HOST & >> $LOG 2>&1
+	  fi
+	  export CPID=$!
+	else
+	  export CPID=-1
+	fi
+	;;
     NX)
-        if [ "$UNAME" != "" ];
-        then
-            UNP="-u $UNAME"
-        else
-            UNP=""
-        fi
-        if [ "$3" != "password" ];
-        then
-            echo "TLC:pubkey"
+	if [ "$UNAME" != "" ];
+	then
+	    UNP="-u $UNAME"
+	else
+	    UNP=""
+	fi
+	if [ "$4" != "password" ];
+	then
+	    echo "TLC:pubkey"
             # enable AUTOLOGIN=1 in config file!
-            /usr/lib/thinlinc/bin/tlclient -C /etc/tlc-pubkey.cfg -x -h options,controlpanel -u $UNAME $HOST & >> $LOG 2>&1
-        else
-            echo "TLC:password"
-            /usr/lib/thinlinc/bin/tlclient -C /etc/tlc-password.cfg -x -h options,controlpanel -u $UNAME $HOST & >> $LOG 2>&1
-        fi 
-        CPID=$!
-        sleep 1 
-        ps | grep -v grep | grep tlclient.bin | awk '{ print $1; }' > /tmp/ut.pid
-        ;;
+	    $TLCBIN/thinlinc/bin/tlclient -x -h options,controlpanel $UNP -C /etc/tlc-pubkey.cfg $HOST & >> $LOG 2>&1
+	    export CPID=$!
+	else
+	    echo "TLC:password"
+	    $TLCBIN/thinlinc/bin/tlclient -x -h options,controlpanel $UNP -C /etc/tlc-password.cfg $HOST & >> $LOG 2>&1
+	    export CPID=$!
+	fi
+        # it looks like login was o.k., remove user key
+	;;
     APP)
-#       $UTBIN/apprun.sh "$APP" "$UNAME" "$HOST" "$PID" & >> $LOG
-        $APP & >> $LOG 2>&1
-        CPID=$!
-        echo $CPID > /tmp/ut.pid
-        ;;
+	$APP & >> $LOG 2>&1
+	export CPID=$!
+	;;
 esac
 
-sleep 5
-rm -f /tmp/user.key
-CPID=`cat /tmp/ut.pid`
-ps >> $LOG 2>&1
-echo "Childprocess started as $CPID over $MPID" >> $LOG 2>&1
-while [ "`ps ax | grep $CPID | grep -v grep > /dev/null; echo $?`" = "0" ]; do
-    sleep 1
-done
+if [ "$CPID" != "-1" ];
+then
 
-echo "exit type=$ETYPE" >> $LOG 2>&1
+    # bad idead, should be based on connection established "signal"
+    sleep 15
+    rm -f /tmp/user.key
 
-if [ "$ETYPE" = "0" ];
-then 
-  SDN="cn=$PID,ou=utsession,ou=utdata,o=fsr,dc=de"
-  echo "Session $CPID closed, removing $SDN" >> $LOG 2>&1
-  ldapdelete -x -y $UTBIN/.pwd -h $LDAP -D "cn=manager,o=fsr,dc=de" $SDN >> $LOG 2>&1
+    echo "Childprocess started as $CPID over $MPID" >> $LOG 2>&1
+
+    export RUNNING=0
+    while [[ $RUNNING -eq 0 && $ETYPE -eq 0 ]];
+    do
+       sleep 1
+       export RUNNING=`kill -0 $CPID &> /dev/null; echo $?`
+#       echo "running: $RUNNING etype=$ETYPE" >> $LOG 2>&1
+    done
+
+    echo "exit-type=$ETYPE, running=$RUNNING" >> $LOG 2>&1
+
+    if [ $ETYPE -eq 0 ];
+    then
+	SDN="cn=$PID,ou=utsession,ou=utdata,o=fsr,dc=de"
+	echo "Session $CPID closed, removing $SDN" >> $LOG 2>&1
+	ldapdelete -x -y $UTBIN/.pwd -h $LDAP -D "cn=manager,o=fsr,dc=de" $SDN >> $LOG 2>&1
+    fi
 fi
 
+echo "utrun.sh: exiting" >> $LOG 2>&1
 exit 0
-
